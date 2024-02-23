9db94e2f4a5457fa031850beefac50a051fef329
Mirja Pirinen <mirja.pirinen@hotmail.com>
2024-02-23 13:34:58 +0200

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Jotain uutta: Hello {value}</div>;
+  return <div>Jotain uutta******: Hello {value}</div>;
 }
 
 export default App;
